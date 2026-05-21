ae63e6dd75f7c3206a3148bf344eefe4e23f6756
Josshua David <2shuadavi@gmail.com>
2026-05-21 14:54:34 -0600

    Primer commit: Estructura inicial del proyecto y chat en JavaFX

---
+++ src/controller/chat_controller.java
@@ -0,0 +1,63 @@
+package controller;
+
+
+import java.net.*;
+import java.io.*;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class chat_controller {
+	@FXML
+	private TextField txt_mensaje;
+	
+	@FXML
+	private TextArea txt_chat;
+	
+	@FXML
+	private Button btn_enviar;
+	
+	private Socket socket;
+	private ServerSocket serversocket;
+	private DataInputStream dataint;
+	private DataOutputStream dataout;
+	
+	public void initialize() {
+		Thread hiloEscucha = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					serversocket = new ServerSocket(5000);
+					socket = serversocket.accept();
+					dataint = new DataInputStream(socket.getInputStream());
+					dataout = new DataOutputStream(socket.getOutputStream());
+					while (true) {
+						String mensaje = dataint.readUTF();
+						Platform.runLater(() -> {
+							txt_chat.appendText(mensaje + "\n");
+						});		
+					}
+					
+				} catch (Exception e) {
+
+				}
+				
+			}
+		});
+		hiloEscucha.start();
+	}
+
+
+	public void enviarMensaje() {
+		try {
+			dataout.writeUTF(txt_mensaje.getText());
+			txt_chat.appendText("Yo: " + txt_mensaje.getText() + "\n");
+			txt_mensaje.clear();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
+
